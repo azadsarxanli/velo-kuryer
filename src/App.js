e0a514a38fcbebdc2bbda112c8b31b4d23cb27ab
azadsarxanli <azad.sarxanli@gmail.com>
2022-03-11 11:45:33 +0400

    track courier section done

--- src/App.js
+++ src/App.js
@@ -202,8 +202,20 @@ function App() {
         </aside>
       </section>
       <section className="tracker">
-        <aside>
+        <aside className="img-aside">
           <img src={trackKuryer} alt="track-kuryer" />
+        </aside>
+        <aside className="content-aside">
+          <div className="content">
+            <h4>Трекер курьера </h4>
+            <p>
+              Nunc hendrerit cursus molestie interdum nulla nulla. Magna duis
+              quis elit, rhoncus amet, eu duis condimentum purus. Eu sit
+              dignissim ut ipsum vel scelerisque id ac. Turpis donec dolor
+              vulputate et iaculis faucibus. Tempus urna enim maecenas feugiat
+              amet, ultricies sit massa. Vestibulum erat.
+            </p>
+          </div>
         </aside>
       </section>
     </div>
