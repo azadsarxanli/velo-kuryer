3b9d1127185854d1daf940a4338276b8de4be62a
azadsarxanli <azad.sarxanli@gmail.com>
2022-03-12 16:21:24 +0400

    slider component is mounted

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Map from "./components/Map";
 import Cards from "./components/Cards";
 import Reason from "./components/Reason";
 import Track from "./components/Track";
+import Slider from "./components/Slider";
 
 function App() {
   return (
@@ -17,88 +18,7 @@ function App() {
       <Cards />
       <Reason />
       <Track />
-      <section className="slider-section">
-        <h4 className="section-title">Часто задаваемые вопросы</h4>
-        <div className="slider-container">
-          <div className="sliders">
-            <div className="slider active">
-              <div className="description">
-                <div className="description-title">
-                  <p> Leo eu donec nibh morbi magna tellus?</p>
-                  <i>
-                    <svg
-                      width="7"
-                      height="4"
-                      viewBox="0 0 7 4"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M0.8225 0L3.5 2.47233L6.1775 0L7 0.761134L3.5 4L0 0.761134L0.8225 0Z"
-                        fill="#2F2F38"
-                      />
-                    </svg>
-                  </i>
-                </div>
-                <div className="description-content">
-                  Sit tellus enim donec quis placerat scelerisque nibh nulla
-                  tortor. Dis pulvinar velit elementum semper lobortis. Odio
-                  quam cursus cursus sit elementum. Orci viverra congue interdum
-                  ac ipsum venenatis, amet amet non. Enim egestas.
-                </div>
-              </div>
-            </div>
-            <div className="slider">
-              <div className="description">
-                <div className="description-title">
-                  <p> Leo eu donec nibh morbi magna tellus?</p>
-                  <i>
-                    <svg
-                      width="7"
-                      height="4"
-                      viewBox="0 0 7 4"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M0.8225 0L3.5 2.47233L6.1775 0L7 0.761134L3.5 4L0 0.761134L0.8225 0Z"
-                        fill="#2F2F38"
-                      />
-                    </svg>
-                  </i>
-                </div>
-              </div>
-            </div>
-            <div className="slider">
-              <div className="description">
-                <div className="description-title">
-                  <p> Leo eu donec nibh morbi magna tellus?</p>
-                  <i>
-                    <svg
-                      width="7"
-                      height="4"
-                      viewBox="0 0 7 4"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M0.8225 0L3.5 2.47233L6.1775 0L7 0.761134L3.5 4L0 0.761134L0.8225 0Z"
-                        fill="#2F2F38"
-                      />
-                    </svg>
-                  </i>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="dots">
-            <div className="dot active"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
-          </div>
-        </div>
-      </section>
+      <Slider />
       <section className="download-app">
         <aside className="app-store-container">
           <div className="app-store">
